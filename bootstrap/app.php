63db288c889be634d31dbffc3c5c4e4131faf2c0
mohabzalat <mohabzalat20@gmail.com>
2025-01-05 18:47:26 +0200

    creating new bootstrap function.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,30 +3,26 @@
 define('BASE_PATH', __DIR__ . '/..'); 
 
 require_once BASE_PATH . '/vendor/autoload.php';
-
 require_once BASE_PATH . '/src/core/App.php';
 require_once BASE_PATH . '/src/core/Database.php';
 
 
 use Dotenv\Dotenv;
 use App\App;
-use App\Database;
+
 
 $dotenv = Dotenv::createImmutable(__DIR__.'/..'); 
 $dotenv->load();
 
-// config files
-$APP_CONFIG = require BASE_PATH . '/config/app.php';
-
 //create app init
 $app = App::init();
+$app->bootstrap();
 
-// making database
-$database = new Database();
+// database start
 
-// registeration
-$app->registerConfig($APP_CONFIG);
+$database = $app->getService('database');
+$orm = $database->orm();
 
-//create database querybuilder
+//crud test
 
 return $app;
