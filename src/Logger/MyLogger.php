665c98f0fae5eb4c0168decc1a2d663936fd73f7
mohabzalat <mohabzalat20@gmail.com>
2025-01-07 18:48:52 +0200

    creating monolog logger

---
+++ src/Logger/MyLogger.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Logger;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+class MyLogger {
+
+    private static $instance = null;
+
+    public static function init(){
+        if(self::$instance == null){
+            $logger = new Logger('app');
+            $logFile = BASE_PATH . '/src/logger/app.log';
+            $logger->pushHandler(new StreamHandler($logFile, Logger::DEBUG));
+            self::$instance = $logger;
+        }
+        return self::$instance;
+    }
+}
