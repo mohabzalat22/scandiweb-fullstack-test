394f0672844a6831f4a4fc1bdcfe31e6dab79db2
mohabzalat <mohabzalat20@gmail.com>
2025-01-08 03:05:27 +0200

    creating query for product by id

--- src/Graphql/Types/QueryType.php
+++ src/Graphql/Types/QueryType.php
@@ -8,19 +8,48 @@ use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 use App\Graphql\Types\ProductType;
 use App\Models\Product;
+use Exception;
+use Throwable;
 
 final class QueryType extends ObjectType {
 
     public function __construct()
     {
+        $productType = new ProductType();
         parent::__construct([
             'name' => 'Query',
             'fields' => [
+                'echo' => [
+                    'type' => Type::string(),
+                    'args' => [
+                        'message' => ['type' => Type::string()],
+                    ],
+                    'resolve' => static fn ($rootValue, array $args): string => $rootValue['prefix'] . $args['message'],
+                ],
                 'products' => [
-                    'type' => Type::listOf(new ProductType()),
+                    'type' => Type::listOf($productType),
                     'resolve' => function($root, $args){
                         return (new Product)->all();
                     },  
+                ],
+                'product' => [
+                    'type' => Type::getNullableType($productType),
+                    'args' => [
+                        'id' => [
+                            'type' => Type::nonNull(Type::string())
+                        ]
+                    ],
+                    'resolve' => function($root, $args){
+                        try{
+                            $product = (new Product)->find($args['id']);
+                            if(!$product || empty($product)){
+                                throw new Exception('unable to find product with id :'.$args['id']);
+                            }
+                            return $product;
+                        } catch (Throwable $e){
+                            \App\App::init()->getService('logger')->error($e->getMessage());
+                        }
+                    }
                 ]
             ],
         ]);
