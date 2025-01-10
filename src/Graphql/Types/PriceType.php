2e82c3ff7c422c20316c407705cf95525de48c27
mohabzalat <mohabzalat20@gmail.com>
2025-01-10 22:19:30 +0200

    creating price type

---
+++ src/Graphql/Types/PriceType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Graphql\Types;
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
+final class PriceType extends ObjectType {
+
+    public function __construct()
+    {
+        parent::__construct([
+            'name' => 'Price',
+            'fields' => [
+                'id' => Type::nonNull(Type::int()),
+                'amount' => Type::string(),
+                'currency' => new CurrencyType
+            ],
+        ]);
+    }
+}
