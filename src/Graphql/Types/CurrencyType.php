00736a38098b9672044c84ce7e577f96ce322300
mohabzalat <mohabzalat20@gmail.com>
2025-01-10 22:19:47 +0200

    creating currency type

---
+++ src/Graphql/Types/CurrencyType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Graphql\Types;
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
+final class CurrencyType extends ObjectType {
+
+    public function __construct()
+    {
+        parent::__construct([
+            'name' => 'Currency',
+            'fields' => [
+                'id' => Type::nonNull(Type::int()),
+                'label' => Type::string(),
+                'symbol' => Type::string(),
+            ],
+        ]);
+    }
+}
