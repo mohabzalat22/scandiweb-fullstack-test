dace625012c028b49ab3582b449402ddab8a5b65
mohabzalat <mohabzalat20@gmail.com>
2025-01-18 21:49:38 +0200

    adding redux provider and apollo provider to main.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import client from "./graphql/client";
+import { ApolloProvider } from '@apollo/client';
+import store from './rtk/store';
+import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>,
+  </StrictMode>
+);
