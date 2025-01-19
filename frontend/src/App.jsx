f7ca60bffc76c827b1d27551603c6f4d66296ac4
mohabzalat <mohabzalat20@gmail.com>
2025-01-19 16:59:31 +0200

    adding error page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Category/>}></Route>
         <Route path="/product/:id" element={<ProductDetails/>}></Route>
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
