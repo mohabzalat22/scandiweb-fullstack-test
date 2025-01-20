b8ed440daac2c1127234302730a7c13300e65ca8
mohabzalat <mohabzalat20@gmail.com>
2025-01-20 20:51:19 +0200

    Creating order item type fields

--- src/Graphql/Types/OrderItemType.php
+++ src/Graphql/Types/OrderItemType.php
@@ -1,13 +1,23 @@
 <?php
-use GraphQL\Type\Definition\ObjectType;
 
-final final class OrderItemType extends ObjectType 
+namespace App\Graphql\Types;
+
+use GraphQL\Type\Definition\InputObjectType;
+use GraphQL\Type\Definition\Type;
+
+final class OrderItemType extends InputObjectType 
 {
     public function __construct()
     {
         parent::__construct([
             'name' => 'OrderItem',
-            'fields' => []
+            'fields' => [
+                'order_id' => Type::int(),
+                'product_id' => Type::int(),
+                'attributes' => Type::string(),
+                'quantity' => Type::int(),
+                'price' => Type::float(),
+            ]
         ]);
     }
 }
