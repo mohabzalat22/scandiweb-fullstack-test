e5f65a83a2c139af543d020ec42a653ff5833a70
mohabzalat <mohabzalat20@gmail.com>
2025-01-20 20:50:04 +0200

    Adding mutation type fields

--- src/Graphql/Types/MutationType.php
+++ src/Graphql/Types/MutationType.php
@@ -1,13 +1,43 @@
 <?php
-use GraphQL\Type\Definition\ObjectType;
 
-final final class MutationType extends ObjectType 
+namespace App\Graphql\Types;
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
+use App\Graphql\Types\OrderType;
+use App\Graphql\Types\OrderItemType;
+
+final class MutationType extends ObjectType
 {
     public function __construct()
     {
         parent::__construct([
             'name' => 'Mutation',
-            'fields' => []
+            'fields' => [
+                'sum' => [
+                    'type' => Type::int(),
+                    'args' => [
+                        'x' => ['type' => Type::int()],
+                        'y' => ['type' => Type::int()],
+                    ],
+                    'resolve' => static fn($calc, array $args): int => $args['x'] + $args['y'],
+                ],
+                'createOrder' => [
+                    'type' => Type::string(),
+                    'args' => [
+                        'order' => [
+                            'type' => new OrderType()
+                        ]
+                    ],
+
+                    'resolve' => function ($root, $args) {
+                        $order = $args['order'];
+                        return json_encode($order);
+                    }
+
+                ]
+            ]
         ]);
     }
 }
