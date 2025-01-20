ac163d251f19421939154fc622ada8e6840dac3c
mohabzalat <mohabzalat20@gmail.com>
2025-01-20 02:43:57 +0200

    adding menu reducer

--- frontend/src/rtk/store.js
+++ frontend/src/rtk/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoryReducer from './slices/categorySlice';
+import menuReducer from './slices/menuSlice';
 
 const store = configureStore({
     reducer: {
         category: categoryReducer,
+        menu: menuReducer
       },
 });
 
