5842e198e6fd828c94241beb6a4291a5c0b8f3e7
mohabzalat <mohabzalat20@gmail.com>
2025-01-22 23:38:04 +0200

    Fixing graphlql uri apollo-client

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -3,6 +3,7 @@ import { ApolloClient , InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
     uri: 'http://localhost/graphql',
     cache: new InMemoryCache(),
+    
 });
 
 export default client;
