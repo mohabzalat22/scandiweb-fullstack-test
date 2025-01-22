4aeccb89af15100d47c34b1cf09627ddb20d0313
mohabzalat <mohabzalat20@gmail.com>
2025-01-22 22:59:22 +0200

    Rewriting graphql uri

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient , InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost/graphql',
+    uri: 'https://scandiweb-fullstack-test-cvtu.vercel.app/graphql',
     cache: new InMemoryCache(),
 });
 
