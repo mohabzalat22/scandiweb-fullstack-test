392bd6beedc8dafe73bfa80eb8e6ac0a242f2eee
mohabzalat <mohabzalat20@gmail.com>
2025-01-22 23:27:57 +0200

    FIxing apollo-client uri

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient , InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'https://scandiweb-fullstack-test-cvtu.vercel.app/graphql',
+    uri: 'https://localhost/graphql',
     cache: new InMemoryCache(),
 });
 
