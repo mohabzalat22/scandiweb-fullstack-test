67f8f9c76625df15162ea8689457e3c6eea40430
mohabzalat <mohabzalat20@gmail.com>
2025-01-22 02:21:40 +0200

    Adding Success order page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
 import "./App.css";
-import Header from "./layout/Header";
 import Category from "./pages/Category";
 import ProductDetails from "./pages/ProductDetails";
 import Error from "./pages/Error";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import SuccessOrder from "../src/components/SuccessOrder"
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Category/>}></Route>
         <Route path="/product/:id" element={<ProductDetails/>}></Route>
+        <Route path="/success" element={<SuccessOrder />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
