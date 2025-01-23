d7f2a5d2b17fd28814d62c4e5c38c61f885d9c92
mohabzalat <mohabzalat20@gmail.com>
2025-01-23 15:13:32 +0200

    Refactoring app after changing name of product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import Category from "./pages/Category";
-import ProductDetails from "./pages/ProductDetails";
 import Error from "./pages/Error";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SuccessOrder from "../src/components/SuccessOrder"
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Category/>}></Route>
-        <Route path="/product/:id" element={<ProductDetails/>}></Route>
+        <Route path="/product/:id" element={<ProductDetailsPage/>}></Route>
         <Route path="/success" element={<SuccessOrder />} />
         <Route path="*" element={<Error />} />
       </Routes>
