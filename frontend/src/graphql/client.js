50a28a762b30f9e5079e67f5bc1071223daba55f
mohabzalat <mohabzalat20@gmail.com>
2025-01-24 14:33:43 +0200

    Running Build again for my ip

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,9 +1,8 @@
 import { ApolloClient , InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost/graphql',
+    uri: 'http://102.43.64.108/graphql',
     cache: new InMemoryCache(),
-    
-});
+}); 
 
 export default client;
