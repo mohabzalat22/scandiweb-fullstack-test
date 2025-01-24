ee4f7ce6ba444e2ab9cdd57c3d6ab6f61d1cde70
mohabzalat <mohabzalat20@gmail.com>
2025-01-24 15:23:09 +0200

    Reverting uri

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,10 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://scanditest.a0001.net/graphql",
-  fetchOptions: {
-    mode: "no-cors", // Disable CORS enforcement
-  },
+  uri: "http://localhost/graphql",
   cache: new InMemoryCache(),
 });
 
