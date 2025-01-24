f5fc03db45c2a4b8d6d614628dd347669ced5372
mohabzalat <mohabzalat20@gmail.com>
2025-01-24 14:34:38 +0200

    Fixing url

---
+++ app.php
@@ -0,0 +1,20 @@
+<?php
+
+define('BASE_PATH', __DIR__ . '/..');
+
+require_once BASE_PATH . '/vendor/autoload.php';
+require_once BASE_PATH . '/src/core/App.php';
+require_once BASE_PATH . '/src/core/Database.php';
+
+
+use Dotenv\Dotenv;
+use App\App;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+//create app init
+$app = App::init();
+$app->bootstrap();
+
+return $app;
--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient , InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://102.43.64.108/graphql',
+    uri: 'http://localhost/graphql',
     cache: new InMemoryCache(),
 }); 
 
