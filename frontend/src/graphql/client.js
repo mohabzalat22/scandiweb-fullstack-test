db8fe36bda316ad2dd45e3197e14790cab72ed3b
mohabzalat <mohabzalat20@gmail.com>
2025-01-24 20:47:52 +0200

    Building After disable cors

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://scanditest.a0001.net/graphql",
   cache: new InMemoryCache(),
+  fetchOptions: {
+    mode: "no-cors",
+  },
 });
 
 export default client;
