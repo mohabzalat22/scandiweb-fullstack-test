d55a65d9ed105b0023c33c113916a81d5cad3b31
mohabzalat <mohabzalat20@gmail.com>
2025-01-24 16:36:19 +0200

    Rebuilding uri

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost/graphql",
+  uri: "https://scanditest.a0001.net/graphql",
   cache: new InMemoryCache(),
 });
 
