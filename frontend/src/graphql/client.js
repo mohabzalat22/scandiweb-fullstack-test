495da6a6f98860583f0cdc9bb726d6a2f0e48e8e
mohabzalat <mohabzalat20@gmail.com>
2025-01-24 14:35:21 +0200

    Revert "Fixing url"
    This reverts commit f5fc03db45c2a4b8d6d614628dd347669ced5372.

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient , InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'http://localhost/graphql',
+    uri: 'http://102.43.64.108/graphql',
     cache: new InMemoryCache(),
 }); 
 
